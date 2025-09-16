991ef3f96113b5c18938a067012f26909ed083b8
Maluleke-Khensani <165706638+Maluleke-Khensani@users.noreply.github.com>
2025-09-16 11:46:28 +0200

    Lecturer Model and Bootstrap styling
    -Implemented:
    *Lecturer- stores the the lecturer's attributes and other claim fields
    * LecturerDatabase- stores the data in-memory list
    *WWWRoot- added styling to be used in all the views

---
+++ Claims_System/Models/Lecturers/Lecturer.cs
@@ -0,0 +1,25 @@
+﻿namespace Claims_System.Models.Lecturers
+{
+    public class Lecturer
+    {
+        public string FullName { get; set; }   
+        public int EmployeeNumber { get; set; } 
+        public string Department { get; set; }   
+       
+        public string Submitted { get; set; } // Date of submission
+        public decimal hoursWorked { get; set; }
+        public decimal Rate = 500.00M;
+        public string status { get; set; } // e.g. "Pending", "Approved", "Rejected"
+        public string Month { get; set; }
+        public double Amount { get; set; }
+
+        public double CalculateAmount()
+        {
+            return (double)(hoursWorked * Rate);
+        }
+        public string CoordinatorStatus { get; set; } 
+        public string ManagerStatus { get; set; } 
+
+    }
+}
+
