357faf13d7a6fb9f1aca3aa36915e178e8c5b177
Maluleke-Khensani <165706638+Maluleke-Khensani@users.noreply.github.com>
2025-09-16 14:39:38 +0200

    Create Lecturer Controller and Index View
    Implemented:
    * Lecturer Controller- added Get Methods for  Index, Create and Edit action with
    *Craeted the Index View- shows the main table with a filter, a count of each type of record

---
+++ Claims_System/Controllers/LecturerController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Claims_System.Models.Lecturers;
+
+namespace Claims_System.Controllers
+{
+    public class LecturerController : Controller
+    {
+        //Created a private field to hold the instance of LecturerDatabase
+        //readonly to ensure it is only set in the constructor
+
+        private readonly LecturerDatabase _lecturerDatabase;
+
+        public LecturerController()
+        {
+           
+            _lecturerDatabase = new LecturerDatabase();
+        }
+       
+        public IActionResult Index()
+        {
+            var lecturers = _lecturerDatabase.GetAllLecturers();
+
+            return View(lecturers);
+        }
+        public IActionResult ClaimForm()
+        {
+            return View();
+        }
+
+        [HttpGet("Lecturer/ViewClaim/{employeeNumber}")]
+
+        public IActionResult ViewClaim(int employeeNumber)
+        {
+            var lecturer = _lecturerDatabase.GetLecturerByEmployeeNo(employeeNumber);
+
+            if (lecturer == null)
+            {
+                return NotFound(); // if not found, return 404
+            }
+
+            return View(lecturer); // pass lecturer object into the View
+        }
+
+        public IActionResult DeleteClaim(int employeeNumber)
+        {
+            var lecturer = _lecturerDatabase.GetLecturerByEmployeeNo(employeeNumber);
+            if (lecturer == null)
+            {
+                return NotFound();
+            }
+
+            // Prototype only – no actual deletion yet
+            return View(lecturer);
+        }
+
+
+        public IActionResult EditClaim(int employeeNumber )
+        {
+            var lecturer = _lecturerDatabase.GetLecturerByEmployeeNo(employeeNumber);
+            if (lecturer == null)
+            {
+                return NotFound(); // if not found, return 404
+            }
+            return View(lecturer); // pass lecturer object into the View
+        }
+
+    }
+}
