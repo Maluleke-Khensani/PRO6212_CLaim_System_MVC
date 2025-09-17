b63871e9efefbd616fb7cc15293f7fd8b1bd4da0
Maluleke-Khensani <165706638+Maluleke-Khensani@users.noreply.github.com>
2025-09-17 16:25:28 +0200

    Create a home page, updated Views
    Added a navigation system in the home page
    Updated the Academic and Coordinator view by separating the tables by status into tabs for easier navigation

---
+++ Claims_System/Controllers/CoordinatorController.cs
@@ -0,0 +1,35 @@
+﻿using Claims_System.Models.Lecturers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Claims_System.Controllers
+{
+    public class CoordinatorController : Controller
+    {
+          private readonly LecturerDatabase _lecturerDatabase;
+
+            public CoordinatorController()
+            {
+                _lecturerDatabase = new LecturerDatabase();
+            }
+
+            public IActionResult Index()
+            {
+                var lecturers = _lecturerDatabase.GetAllLecturers();
+                return View(lecturers);
+            }
+
+            public IActionResult ReviewClaim2(int employeeNumber)
+            {
+                var claim = _lecturerDatabase.GetLecturerByEmployeeNo(employeeNumber);
+
+                if (claim == null)
+                {
+                    return NotFound(); // if not found, return 404
+                }
+
+                return View(claim); // pass lecturer object into the View
+            }
+
+        }
+    }
+
