b7990e7b3689773985922fb80cb1fff77f5b0333
Maluleke-Khensani <st10451309@imconnect.edu.za>
2025-10-19 21:06:26 +0200

    Add  DbContext, and initial migration
    Added validation attributes (Required, Range, DataType)
    - Created ClaimsDbContext with DbSet<LecturerClaim>
    - Added EF Core initial migration to generate LecturerClaims table

---
+++ Claims_System/Migrations/20251019090828_LecturerInitial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Claims_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class LecturerInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LecturerClaims",
+                columns: table => new
+                {
+                    ClaimId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeNumber = table.Column<int>(type: "int", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModuleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Submitted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HoursWorked = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Rate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CoordinatorStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ManagerStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Document1FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Document1FileData = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Document2FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Document2FileData = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LecturerClaims", x => x.ClaimId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LecturerClaims");
+        }
+    }
+}
---
+++ Claims_System/Models/ClaimsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Claims_System.Models
+{
+    public class ClaimsDbContext : DbContext
+    {
+        public ClaimsDbContext(DbContextOptions<ClaimsDbContext> options) : base(options) { }
+
+        public DbSet<LecturerClaim> LecturerClaims { get; set; }
+    }
+}
